6978ede4c45b304b779c0d33e58db97d7c4532cc
Konstantin Christ <117539303+Konstantinchrist@users.noreply.github.com>
2022-12-17 20:08:43 +0100

    New projects

--- Junior/TIp-Calculator/script.js
+++ Junior/TIp-Calculator/script.js
@@ -9,9 +9,10 @@ const _5 = document.getElementById('five');
 const _10 = document.getElementById('ten');
 const _15 = document.getElementById('fifteen');
 const _20 = document.getElementById('twenty');
-const _25 = document.getElementById('twenty-five');
 const _50 = document.getElementById('fifty');
-const tips = Array.from(document.getElementsByClassName('tip'));
+const customTip = document.getElementById('custom-tip'); 
+const tips = Array.from(document.querySelectorAll('.tip'));
+
 
 bill.value='0.00';
 people.value='1';
@@ -23,75 +24,101 @@ let tipValue = 0.00;
 bill.addEventListener('input', reloadAll);
 people.addEventListener('input', reloadAll);
 
-function resetTipStyles(){
-    element.style.backgroundColor='hsl(183, 100%, 15%)';
+function resetTipStyles(element){
+    element.style.backgroundColor = 'hsl(183, 100%, 15%)';
+    console.log('resetTipStyles');
+}
+
+function resetAll(){
+    tipValue = 0;
+    bill.value='0.00';
+    people.value='1';
+    customTip.value = '';
+    tipPerPerson.innerHTML=('$0.00');
+    totalPerPerson.innerHTML=('$0.00');
+    error.style.display='none';
+    people.style.borderColor='hsl(185, 41%, 84%)';
 }
 
 
 function reloadAll(){
-    if (! people.value==''){
+    if (! people.value == 0){
+        console.log(parseFloat(people.value))
         if (bill.value == ''){
             billValue = 0.00;
+            bill.value='0.00';
+            people.value='1';
         }
         else{
             error.style.display='none';
             let billValue = parseFloat(bill.value);
             let peopleValue = parseFloat(people.value);
 
-            let tipAmountperPerson = (billValue / peopleValue * tipValue);
+            let tipAmountperPerson = ((billValue / peopleValue * tipValue));
             console.log('Tip per person: $' + tipAmountperPerson);
 
             let totalPerPersonWithTip = ((billValue / peopleValue) + tipAmountperPerson);
             console.log('Total per person: $' + totalPerPersonWithTip);
 
-            tipPerPerson.innerHTML=('$' + (tipAmountperPerson));
-            totalPerPerson.innerHTML=('$' + (totalPerPersonWithTip));
+            tipPerPerson.innerHTML=('$' + (tipAmountperPerson.toFixed(2)));
+            totalPerPerson.innerHTML=('$' + (totalPerPersonWithTip.toFixed(2)));
 
             console.log(peopleValue);
             console.log(billValue);
         }
-        
+        error.style.display='none';
+        people.style.borderColor='hsl(185, 41%, 84%)';
     }
     else{
         error.style.display='block';
+        people.style.borderColor='red';
     }
 }
 
+restartButton.addEventListener('click', () => {
+    resetAll();
+})
 
 _5.addEventListener('click', () => {
     tipValue = 0.05;
+    reloadAll(); tips.forEach(resetTipStyles);
+    _5.style.backgroundColor = 'hsl(172, 56%, 34%)';
     console.log('tip value: ' + tipValue)
 })
 
 _10.addEventListener('click', () => {
     tipValue = 0.1;
+    reloadAll(); tips.forEach(resetTipStyles);
+    _10.style.backgroundColor = 'hsl(172, 56%, 34%)';
     console.log('tip value: ' + tipValue)
 })
 
 _15.addEventListener('click', () => {
     tipValue = 0.15;
+    reloadAll(); tips.forEach(resetTipStyles);
+    _15.style.backgroundColor = 'hsl(172, 56%, 34%)';
     console.log('tip value: ' + tipValue)
 })
 
 _20.addEventListener('click', () => {
     tipValue = 0.20;
-    console.log('tip value: ' + tipValue)
-})
-
-_25.addEventListener('click', () => {
-    tipValue = 0.25;
-    console.log('tip value: ' + tipValue)
+    reloadAll(); tips.forEach(resetTipStyles);
+    _20.style.backgroundColor = 'hsl(172, 56%, 34%)';
+    console.log('tip value: ' + tipValue);
 })
 
 _50.addEventListener('click', () => {
-    tipValue = 0.50;
+    tipValue = 0.50
+    reloadAll(); tips.forEach(resetTipStyles);
+    _50.style.backgroundColor = 'hsl(172, 56%, 34%)';
     console.log('tip value: ' + tipValue)
 })
 
-
-tips.forEach(function(val){
-    resetTipStyles();
-    console.log('add style')
-    val.style.backgroundColor='hsla(172, 56%, 54%, 0.767)';
-    val.addEventListener('click', tipStyles);
+customTip.addEventListener('input', () => {
+    reloadAll(); tips.forEach(resetTipStyles);
+    parseFloat(customTip.value);
+    tipValue = (customTip.value / 100);
+    console.log('tip value: ' + tipValue)
+    reloadAll();
 })
+
