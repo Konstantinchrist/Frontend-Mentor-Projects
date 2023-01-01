933422b0d4a8c683a5ad7936e76b63a84965ea4f
Konstantin Christ <117539303+Konstantinchrist@users.noreply.github.com>
2023-01-01 21:01:49 +0100

    Update script.js

--- Intermediate/Multi-Step-Choice-Summary/script.js
+++ Intermediate/Multi-Step-Choice-Summary/script.js
@@ -1,4 +1,4 @@
-// Variables
+// Variables //
 
 //inputs & errors
 const errorName = document.getElementById('error-name');
@@ -35,9 +35,9 @@ const advanced = document.getElementById('advanced');
 const arcade = document.getElementById('arcade');
 const optionsMode = Array.from(document.getElementsByClassName('option-w2'));
 
-const modeW4 = document.getElementById('mode');
-const priceMode = document.getElementById('mode-price-1');
-const priceArea = document.getElementById('total-price-area');
+const modeOutput = document.getElementById('mode');
+const priceModeOutput = document.getElementById('mode-price-1');
+const priceTotalOutput = document.getElementById('total-price-area');
 
 //add-ons
 const onlineService = document.getElementById('online-service-w3');
@@ -45,9 +45,9 @@ const largerStorage = document.getElementById('larger-storage-w3');
 const customProfile = document.getElementById('custom-profile-w3');
 const optionsAddons = Array.from(document.getElementsByClassName('option-w3'));
 
-const onlineServiceW4 = document.getElementById('online-service-w4');
-const largerStorageW4 = document.getElementById('larger-storage-w4');
-const customProfileW4 = document.getElementById('custom-profile-w4');
+const onlineServiceOutput = document.getElementById('online-service-w4');
+const largerStorageOutput = document.getElementById('larger-storage-w4');
+const customProfileOutput = document.getElementById('custom-profile-w4');
 const choiceAddons = Array.from(document.getElementsByClassName('option-w4'));
 
 //steps sidebar
@@ -57,19 +57,15 @@ const number3 = document.getElementById('step-3');
 const number4 = document.getElementById('step-4');
 const stepNumbers = Array.from(document.querySelectorAll('.sidebar-step-number'));
 
-//counters
+//counters & else
 let step = 1;
 let mode = 'Arcade';
 let modePrice = 9;
-let onlineServiceOption = false;
-let largerStorageOption = false;
-let customProfileOption = false;
-let formValid = false;
 let totalPrice = 0;  
 
-resetAll(); newStep(); chosenOptionStyle(arcade);
+resetAll(); newStep(); addOptionStyle(arcade);
 
-// functions
+// functions (general) //
 function hide(element){
     element.style.display = 'none';
 }
@@ -78,92 +74,49 @@ function show(element){
     element.style.display = 'block';
 }
 
-function currentNumberStyle(element){
-    element.style.backgroundColor='white'; element.style.color='black';
-}
-
-function chosenOptionStyle(element){
-    element.style.borderColor='hsl(243, 100%, 62%)'; element.style.backgroundColor='hsl(231, 100%, 98%)';
-}
-
-// calculate price
-function calculatePrice(){
-    choiceAddons.forEach(hide); totalPrice = 0;
-        if (onlineServiceOption == true){//detect for choice of addons
-            onlineServiceW4.style.display='flex';
-            totalPrice += 1;
-        }
-        if (largerStorageOption == true){//detect for choice of addons
-            largerStorageW4.style.display='flex';
-            totalPrice += 2;
-        }
-        if (customProfileOption == true){//detect for choice of addons
-            customProfileW4.style.display='flex';
-            totalPrice += 2;
-        }
-
-        modeW4.innerHTML = mode;
-        priceMode.innerHTML = '$' + modePrice + '/mo';
-        totalPrice += modePrice;
-        
-        priceArea.innerHTML=('$' + totalPrice + '/mo')
-}
-
-// reset functions
 function resetAll(){
     windows.forEach(hide); 
     buttons.forEach(hide);
-    stepNumbers.forEach(resetSidebar);
-}
-
-function resetSidebar(element){
-    element.style.color='white'; element.style.background='none';
-}
-
-function resetErrors(){
-    inputs.forEach(resetInputStyles); errorsInput.forEach(resetErrorStyles);
-}
-
-function resetInputStyles(element){
-    element.style.border='1px solid black';
-}
-
-function resetErrorStyles(element){
-    element.style.color='white';
-}
-
-function resetOptions(element){
-    element.style.borderColor='black'; element.style.background='none';
-}
-
-function resetInput(element){
-    element.value = '';
+    stepNumbers.forEach(resetNumberStyle);
 }
 
 // window detection and execution
-// new steps detect and show right objects
+// new steps detect and show requested objects
+// step count => show/hide objects
 function newStep(){
     resetAll();
-    switch (step){
+    switch (step){ 
         case 1:
-            show(window1); show(goBack); show(nextStep); currentNumberStyle(number1);
+            show(window1); show(goBack); show(nextStep); addNumberStyle(number1);
             break;
         case 2:
-            show(window2); show(goBack); show(nextStep); currentNumberStyle(number2);
+            show(window2); show(goBack); show(nextStep); addNumberStyle(number2);
             break;
         case 3:
-            show(window3); show(goBack); show(nextStep); currentNumberStyle(number3);
+            show(window3); show(goBack); show(nextStep); addNumberStyle(number3);
             break;
         case 4:
-            show(window4); show(goBack); show(Confirm); currentNumberStyle(number4);
+            show(window4); show(goBack); show(Confirm); addNumberStyle(number4);
             calculatePrice();
             break;
         case 5:
-            show(window5); currentNumberStyle(number4);
+            show(window5); addNumberStyle(number4);
     }
 }
 
-// move windows by sidebar function
+// function for marking "step" at sidebar
+function addNumberStyle(element){
+    element.style.backgroundColor='white'; element.style.color='black';
+}
+
+function resetNumberStyle(element){
+    element.style.color='white'; element.style.background='none';
+}
+
+// navigation //
+
+// navigation by sidebar 
+stepNumbers.forEach(numbersSidebar);
 function numbersSidebar(element){ 
     element.addEventListener('click', () => {
         step = parseFloat(element.innerHTML);
@@ -171,32 +124,30 @@ function numbersSidebar(element){
     })
 }
 
-// move windows by buttons function
+// navigation by buttons
+buttons.forEach(buttonsWindows);
 function buttonsWindows(element){
     element.addEventListener('click', () => {
         switch (element.innerHTML){
-            case 'Go back':
+            case 'Go back': // -1 step
                 if (step > 1){
                     step--;
                 }
                 break;
-
-            case 'Next step':
+            case 'Next step': // +1 step
                 if (step == 1){
                     checkFormValidation();
                     if (! inputName.value == '' &&
                         inputEmail.value.match(patternEmail) &&
                         inputPhone.value.match(patternPhone)){ // detect for invalid name/email/phoneNumber
                         step++;
-                        resetAll(); chosenOptionStyle(arcade);
                     }
                 }
                 else{
                     step++;
                 }
                 break;
-
-            case 'Confirm': 
+            case 'Confirm': // +1 step (finalstep)
                 checkFormValidation();
                 if (! inputName.value == '' &&
                     inputEmail.value.match(patternEmail) &&
@@ -214,73 +165,98 @@ function buttonsWindows(element){
     })
 }
 
-// choose mode
-function chooseMode(element){
-    element.addEventListener('click', () => {
-        optionsMode.forEach(resetOptions);
-        mode = element.querySelector('h2').innerHTML;
-        modePrice = parseFloat(element.querySelector('p').innerHTML.slice(1,3));
-        chosenOptionStyle(element);
-        console.log('mode: ' + mode); console.log('mode price: ' + modePrice); 
-    })
+// window 1/form validation
+
+function resetInputStyles(element){
+    element.style.border='1px solid black';
 }
 
-// move windows by sidebar
-stepNumbers.forEach(numbersSidebar);
-// move windows by buttons
-buttons.forEach(buttonsWindows);
+function resetErrorStyles(element){
+    element.style.color='white';
+}
 
-// window 1/form validation
+function resetInput(element){
+    element.value = '';
+}
+
 function checkFormValidation(){
-    resetErrors();
-    if (inputName.value =='' || ! inputName.value == 'x'){
+    inputs.forEach(resetInputStyles);
+    errorsInput.forEach(resetErrorStyles);
+    if (inputName.value ==''){
         errorName.style.color='red'; 
         inputName.style.border=' 1px solid red';
-        console.log('name invalid');
+        // console.log('name invalid');
     }
     if (! inputEmail.value.match(patternEmail)){
         errorEmail.style.color='red';
         inputEmail.style.border=' 1px solid red';
-        console.log('email invalid');
+        // console.log('email invalid');
     }
     if (! inputPhone.value.match(patternPhone)){
         errorPhone.style.color='red';
         inputPhone.style.border=' 1px solid red';
-        console.log('phone number invalid');
+        // console.log('phone number invalid');
     }
 }
     
+// modes & addons (2/3)
+
+function resetOptionStyle(element){
+    element.style.borderColor='black'; element.style.background='none';
+}
+function addOptionStyle(element){
+    element.style.borderColor='hsl(243, 100%, 62%)'; element.style.backgroundColor='hsl(231, 100%, 98%)';
+}
+
 // window 2/get chosen mode
 optionsMode.forEach(chooseMode);
+function chooseMode(element){
+    element.addEventListener('click', () => {
+        optionsMode.forEach(resetOptionStyle); // styling
+        addOptionStyle(element); // ''
+        mode = element.querySelector('h2').innerHTML; // detect mode
+        modePrice = parseFloat(element.querySelector('p').innerHTML.slice(1,3)); // get price of mode
+        // console.log('mode: ' + mode); console.log('mode price: ' + modePrice); 
+    })
+}
 
 // window 3/get chosen mode
-onlineService.addEventListener('click', () => {
-    if (onlineServiceOption == false){
-        chosenOptionStyle(onlineService);
-        onlineServiceOption = true;
-    }
-    else if (onlineServiceOption == true){
-        resetOptions(onlineService);
-        onlineServiceOption = false;
-    }
-})
-largerStorage.addEventListener('click', () => {
-    if (largerStorageOption == false){
-        chosenOptionStyle(largerStorage);
-        largerStorageOption = true;
-    }
-    else if (largerStorageOption == true){
-        resetOptions(largerStorage);
-        largerStorageOption = false;
-    }
-})
-customProfile.addEventListener('click', () => {
-    if (customProfileOption == false){
-        chosenOptionStyle(customProfile);
-        customProfileOption = true;
-    }
-    else if (customProfileOption == true){
-        resetOptions(customProfile);
-        customProfileOption = false;
-    }
-})
+optionsAddons.forEach(chooseAddons);
+function chooseAddons(element){ // function for detecting addons and performing their styling
+    element.value = false;
+    element.addEventListener('click', () => {
+        if (element.value == false){
+            addOptionStyle(element);
+            element.value = true;
+        }
+        else{
+            resetOptionStyle(element);
+            element.value = false;
+        }
+        // console.log(element.value)
+    })
+}
+
+// window 4/price summary & else
+
+// calculate price
+function calculatePrice(){
+    choiceAddons.forEach(hide);
+    totalPrice = 0;
+        if (onlineService.value == true){ // detect for choice of addons
+            onlineServiceOutput.style.display='flex';
+            totalPrice += 1;
+        }
+        if (largerStorage.value == true){ // detect for choice of addons
+            largerStorageOutput.style.display='flex';
+            totalPrice += 2;
+        }
+        if (customProfile.value == true){ // detect for choice of addons
+            customProfileOutput.style.display='flex';
+            totalPrice += 2;
+        }
+        modeOutput.innerHTML = mode; // output chosen mode
+        priceModeOutput.innerHTML = '$' + modePrice + '/mo'; // output price of chose mode
+        totalPrice += modePrice; // calculate final total price
+        priceTotalOutput.innerHTML=('$' + totalPrice + '/mo') // output total price
+}
